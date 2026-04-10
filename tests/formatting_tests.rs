b9f470604f4bf08d236b1ad1734e3f0994015583
Takeshi <152513888+0xhokugava@users.noreply.github.com>
2026-04-10 19:40:04 +0630

    fix formatting tests

--- tests/formatting_tests.rs
+++ tests/formatting_tests.rs
@@ -5,16 +5,16 @@ use quantum_lab::formatting::to_dirac;
 fn test_to_dirac_formatting() {
     // 1 Qubit: length 2, binary width should be 1 (|0>)
     let q1 = array![1.0, 0.0];
-    assert_eq!(to_dirac(&q1), " +1.000|0>");
+    assert_eq!(to_dirac(&q1), "+1.000|0>");
 
     // 2 Qubits: length 4, binary width should be 2 (|00>)
     let q2 = array![0.0, 0.0, 1.0, 0.0]; // state |10>
-    assert_eq!(to_dirac(&q2), " +1.000|10>");
+    assert_eq!(to_dirac(&q2), "+1.000|10>");
 
     // 3 Qubits: length 8, binary width should be 3 (|000>)
     let mut q3 = Array1::zeros(8);
     q3[7] = 1.0; // state |111>
-    assert_eq!(to_dirac(&q3), " +1.000|111>");
+    assert_eq!(to_dirac(&q3), "+1.000|111>");
 }
 
 #[test]
